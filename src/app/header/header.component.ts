a388d7449b2bb7738f6bc300ab91af32497d2fe7
kursad333 <45723522+kursad333@users.noreply.github.com>
2020-10-28 15:24:25 +0100

    added endpoint for articles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, getNumberOfCurrencyDigits } from '@angular/common';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,14 @@ export class HeaderComponent implements OnInit {
   constructor(private httpClient: HttpService) { }
 
   article_count: string;
-
-  ngOnInit(): void {
-
+  public getCount(): void{
+    var article_count;
     this.httpClient.fetchStatistics().subscribe(data => {
       this.article_count = data['query']['statistics']['articles'].toString()
     }) 
+  }
+
+  ngOnInit(): void {
+    this.getCount()
   }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -13,10 +13,14 @@ export class HttpService {
   constructor(private httpClient: HttpClient) { }
 
   private statistics_endpoint = 'https://en.wikipedia.org/w/api.php?action=query&meta=siteinfo&siprop=statistics&format=json&origin=*';
-  private article_endpoint = ''
+  private article_endpoint = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=extracts|info&exintro&inprop=url&origin=*&formatversion=2&exintro'
 
   public fetchStatistics(){
     return this.httpClient.get(this.statistics_endpoint)
+  }
+
+  public fetchArticle(){
+    return this.httpClient.get(this.article_endpoint)
   }
 
 }
