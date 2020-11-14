3a552f7d38b5224ed486d5e485d9e87877631574
kursad333 <45723522+kursad333@users.noreply.github.com>
2020-11-14 17:17:52 +0100

    optmized accessibility

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, enableProdMode, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 
 @Component({
